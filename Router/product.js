5bb81d76e85a5b07454228a75db8042abd051c97
DESKTOP-VLQFQP6\admin <vipul.buoyancy@gmail.com>
2023-08-28 19:55:50 +0530

    update product api and get product api

--- Router/product.js
+++ Router/product.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
+const product = require('../models/product');
 const Product = require('../models/product')
 
 router.get('/getallproducts', async (req, res) => {
     try {
         const products = await Product.find();
         res.status(201).send(products)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+router.get('/getproductbyid/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const product = await Product.findById(id);
+        res.status(201).send(product)
     } catch (err) {
         res.status(500).send(err)
     }
@@ -27,6 +38,24 @@ router.post('/addproduct', async (req, res) => {
         const savedProduct = await createProduct.save();
         res.status(201).send(savedProduct)
     } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+router.put('/updateproduct/:id', (req, res) => {
+    const id = req.params.id;
+    try {
+        product.findByIdAndUpdate(id, req.body, { useFindAndModify: true }).then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Failed to update Product with id=${id}.`
+                });
+            } else res.send({
+                message: "Product was updated successfully."
+            });
+        })
+        // res.status(200).send('updated successfull')
+    } catch (error) {
         res.status(500).send(err)
     }
 })
